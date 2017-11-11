645f25a5ab153e6375e96c0d5e65f995476a1b59
Luiz <luiz.carlos.limajf@gmail.com>
2017-11-11 20:31:25 -0200

    Aula2
    Organização da estrutura inicial e criação dos primeiros componentes

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        <h1>Conteúdo</h1>
+        <p>Conteúdo do componente</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
